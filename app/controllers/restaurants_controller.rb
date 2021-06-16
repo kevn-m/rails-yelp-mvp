084bcb03ffc64a559f15568438d5f4e51a65fa4d
kjam0 <south.kay@gmail.com>
2021-06-16 16:43:06 +1000

    added nested resources

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,37 @@
 class RestaurantsController < ApplicationController
+
+  before_action :find_restaurant, only: [ :show, :review ]
+
+
   def index
     @restaurants = Restaurant.all
   end
 
+  def show; end
+
   def new
     @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def review
+    @restaurant = Restaurant.find(params[:id])
+    @content = @restaurant.content
+    @rating = @restaurant.rating
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit( :name, :address, :category, :phone_number)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,31 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # homepage goes to index
   root to: 'restaurants#index'
 
-  # /restaurants also goes to index
-  get '/restaurants', to: 'restaurants#index'
-  get '/restaurants/new', to: 'restaurants#new'
+  # get '/restaurants/new', to: 'restaurants#new'
+
+
+  # # show one restaurant
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # # show all restaraunts
+  # get '/restaurants', to: 'restaurants#index'
+
+  #  # create a new restaurant
+
+  # post '/restaurants', to: 'restaurants#create'
+
+  resources :restaurants, only: [:new, :index, :show, :create] do
+    member do
+      get :reviews
+    end
+  end
 end
+
+# nest resources
+  # resources :restaurants do
+  #   collection do
+  #     get :reviews
+  #   end
+  # end
