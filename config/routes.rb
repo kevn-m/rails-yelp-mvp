ddf2c461f18ba9e32a93846f86c0b97f5aee178f
kjam0 <south.kay@gmail.com>
2021-06-16 15:31:31 +1000

    added new form in #new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # homepage goes to index
+  root to: 'restaurants#index'
+
+  # /restaurants also goes to index
+  get '/restaurants', to: 'restaurants#index'
+  get '/restaurants/new', to: 'restaurants#new'
 end
